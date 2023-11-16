474e7f942b3aeefce04aba6c6b45e519684f9e26
Slava <plumsoftwareofficial@gmail.com>
2023-11-16 14:55:30 +0600

    Небольшие изменения связанные с базой данных и градле

--- app/src/main/java/ru/plumsoftware/notebook/databases/SQLiteDatabaseManager.java
+++ app/src/main/java/ru/plumsoftware/notebook/databases/SQLiteDatabaseManager.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class SQLiteDatabaseManager extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION = 6;
+    public static final int DATABASE_VERSION = 5;
     public static final String DATABASE_NAME = DatabaseConstants.DATABASE_NAME;
 
     public SQLiteDatabaseManager(Context context) {
@@ -19,15 +19,26 @@ public class SQLiteDatabaseManager extends SQLiteOpenHelper {
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-//        db.execSQL(DatabaseConstants.DELETE_NOTES_TABLE);
-//        db.execSQL(DatabaseConstants.DELETE_GROUPS_TABLE);
-//        onCreate(db);
-        // Добавьте новые столбцы в таблицу notes
-        if (DATABASE_VERSION > 4) {
-            db.execSQL("ALTER TABLE " + DatabaseConstants._NOTES_TABLE_NAME +
-                    " ADD COLUMN " + DatabaseConstants._CHANNEL_ID + " TEXT");
-            db.execSQL("ALTER TABLE " + DatabaseConstants._NOTES_TABLE_NAME +
-                    " ADD COLUMN " + DatabaseConstants._IS_NOTIFY + " INTEGER");
+        if (newVersion > oldVersion) {
+            String tempTable = "temp_table";
+
+            // Создаем временную таблицу и копируем данные
+            db.execSQL("CREATE TABLE " + tempTable + " AS SELECT * FROM " + DatabaseConstants._NOTES_TABLE_NAME);
+
+            // Удаляем исходную таблицу
+            db.execSQL(DatabaseConstants.DELETE_NOTES_TABLE);
+
+            // Создаем новую таблицу с новыми полями
+            db.execSQL(DatabaseConstants.CREATE_NOTES_TABLE);
+
+            db.execSQL("ALTER TABLE " + tempTable + " ADD " + DatabaseConstants._CHANNEL_ID + " TEXT;");
+            db.execSQL("ALTER TABLE " + tempTable + " ADD " + DatabaseConstants._IS_NOTIFY + " INTEGER;");
+
+            // Копируем данные из временной таблицы в новую таблицу
+            db.execSQL("INSERT INTO " + DatabaseConstants._NOTES_TABLE_NAME + " SELECT * FROM " + tempTable);
+
+            // Удаляем временную таблицу
+            db.execSQL("DROP TABLE IF EXISTS " + tempTable);
         }
     }
 
